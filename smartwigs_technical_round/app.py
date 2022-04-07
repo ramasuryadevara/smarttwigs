38e7250475641b3db6470106c12f7a83a3fd218a
ramasuryadevara <suryadevararama123@gmail.com>
2022-04-07 14:32:05 -0700

    interview code uploaded

---
+++ smartwigs_technical_round/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+app.config["DEBUG"] = False
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
